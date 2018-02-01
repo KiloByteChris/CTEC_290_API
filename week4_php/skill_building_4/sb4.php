6ae92bf17fdbdf569b217f80479083a8433034ce
c.mcguire <cdmcguire000@gmail.com>
2018-02-01 10:34:19 -0800

    skillbuilding 4 - php - inital commit

---
+++ week4_php/skill_building_4/sb4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- 
+		Skill Building 4 - API with PHP
+		Chris McGuire
+		CTEC 290 - API
+		Winter 18
+	-->
+	<meta charset="utf-8" />
+	<title>NYT API with PHP</title>
+
+	<!-- CSS -->
+	<link href="css/reset.css" rel="stylesheet" />
+	<link href="css/main.css" rel="stylesheet" />
+
+</head>
+<body>
+	<header>
+	</header>
+	<main>
+		<div id="articleDiv">
+		</div>
+	</main>
+</body>
+</html>
