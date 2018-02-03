d312d133b3f69b88ac4ebfc5a96ed1878c7aa3b8
c.mcguire <cdmcguire000@gmail.com>
2018-02-03 14:11:07 -0800

    quiz 4 - some css

--- Quiz4/js/quiz4.js
+++ Quiz4/js/quiz4.js
@@ -8,6 +8,23 @@ $(document).ready(function(){
 	    console.log(data);
 	    // iterate through the data and create a string to display the JSON
 	    $.each(data.result, function(key, value){
+	    	// display tags
+	    	var tagString = "";
+	    	$.each (value.tags, function(key, value){
+	    		tagString += '<p>'+value+'</p>';
+	    	})
+	    	var linkedideaidString = "";
+	    	// display linkedideaid
+	    	$.each (value.linkedideaid, function(key, value){
+	    		linkedideaidString += '<p>'+value+'</p>';
+	    	})
+	    	var commentString = "";
+	    	//display comments
+	    	$.each(value.comments, function(key, value){
+	    		commentString += '<p>'+value.comment+'</p>'+
+	    		'<p>'+value.createdby+'</p>'+
+	    		'<p>'+value.datecreated+'</p>';
+	    	})
 	    	var displayString = "";
 	    	displayString += "<article>" +
 	    	'<h2><a href='+value.ideaurl+'>'+value.idea+'</a></h2>' +
@@ -22,24 +39,13 @@ $(document).ready(function(){
 	    	'<p>'+value.status+'</p>' +
 	    	'<p>'+value.additionallongtext+'</p>' +
 	    	'<p>'+value.additionaltext+'</p>' +
-	    	'<p>'+value.votes+'</p>';
-	    	// display tags
-	    	$.each (value.tags, function(key, value){
-	    		displayString += '<p>'+value+'</p>';
-	    	})
-	    	// display linkedideaid
-	    	$.each (value.linkedideaid, function(key, value){
-	    		displayString += '<p>'+value+'</p>';
-	    	})
-	    	//display comments
-	    	$.each(value.comments, function(key, value){
-	    		displayString += '<p>'+value.comment+'</p>'+
-	    		'<p>'+value.createdby+'</p>'+
-	    		'<p>'+value.datecreated+'</p>';
-	    	})
-	    	displayString += '<article>';
+	    	'<p>'+value.votes+'</p>'+
+	    	tagString +
+	    	linkedideaidString +
+	    	commentString +
+	    	'</article>';
 	    	// display the displayString
-	    	$('#articleDiv').append(displayString);
+	    	$('#articleSection').append(displayString);
 	    }) 
 	}).fail(function(err) {
 	    throw err;
